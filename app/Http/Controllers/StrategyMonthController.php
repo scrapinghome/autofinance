e23c3eef2e8ba64fd467617b230242c6a60ad6a4
tushar <tushar.khan0122@gmail.com>
2023-03-29 01:02:48 +0600

    'strategy'

---
+++ app/Http/Controllers/StrategyMonthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StrategyMonth;
+use Illuminate\Http\Request;
+
+class StrategyMonthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StrategyMonth  $strategyMonth
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StrategyMonth $strategyMonth)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StrategyMonth  $strategyMonth
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StrategyMonth $strategyMonth)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\StrategyMonth  $strategyMonth
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StrategyMonth $strategyMonth)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StrategyMonth  $strategyMonth
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StrategyMonth $strategyMonth)
+    {
+        //
+    }
+}
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -124,7 +124,7 @@
     </div>
     <!-- Slider Ends -->
     <!-- Features Section Starts -->
-    
+
     <!-- Features Section Ends -->
     <!-- About Section Starts -->
     <section class="about-us">
@@ -434,6 +434,42 @@
         </div>
     </section>
     <!-- Pricing Ends -->
+
+    <!-- Bitcoin Calculator Section Starts -->
+    <section class="bitcoin-calculator-section">
+        <div class="container">
+            <div class="row">
+                <!-- Section Heading Starts -->
+                <div class="col-md-12">
+                    <h2 class="title-head text-center"><span>Bitcoin</span> Calculator</h2>
+                    <p class="message text-center">Find out the current Bitcoin value with our easy-to-use converter</p>
+                </div>
+                <!-- Section Heading Ends -->
+                <!-- Bitcoin Calculator Form Starts -->
+                <div class="col-md-12 text-center">
+                    <form class="bitcoin-calculator" id="bitcoin-calculator">
+                        <!-- Input #1 Starts -->
+                        <input class="form-input" name="btc-calculator-value" value="1">
+                        <!-- Input #1 Ends -->
+                        <div class="form-info"><i class="fa fa-bitcoin"></i></div>
+                        <div class="form-equal">=</div>
+                        <!-- Input/Result Starts -->
+                        <input class="form-input form-input-result" name="btc-calculator-result">
+                        <!-- Input/Result Ends -->
+                        <!-- Select Currency Starts -->
+                        <div class="form-wrap">
+                            <select id="currency-select" class="form-input select-currency select-primary" name="btc-calculator-currency" data-dropdown-class="select-primary-dropdown"></select>
+                        </div>
+                        <!-- Select Currency Ends -->
+                    </form>
+                    <p class="info"><i>* Data updated every 15 minutes</i></p>
+                </div>
+                <!-- Bitcoin Calculator Form Ends -->
+            </div>
+        </div>
+    </section>
+    <!-- Bitcoin Calculator Section Ends -->
+
     <!-- Bitcoin Calculator Section Starts -->
     <section class="bitcoin-calculator-section">
         <div class="container">
